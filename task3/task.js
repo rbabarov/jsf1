b4518920d6e66e7666094cb5a8080d30766a0c4a
rbabarov <90253870+rbabarov@users.noreply.github.com>
2022-09-12 22:03:27 +0500

    2

--- task3/task.js
+++ task3/task.js
@@ -5,13 +5,17 @@ function hasMole(hole){
     if (hole.classList.contains("hole_has-mole")){
         dead.innerHTML = parseInt(dead.innerHTML) + 1;
         if (dead.innerHTML == 5){
-            alert("Победа!")
+            alert("Победа!");
+            lost.innerHTML = 0;
+            dead.innerHTML = 0;
         }
     }
     else {
         lost.innerHTML = parseInt(lost.innerHTML) + 1;
         if (lost.innerHTML == 3){
-            alert("Вы проиграли!")
+            alert("Вы проиграли!");
+            lost.innerHTML = 0;
+            dead.innerHTML = 0;
         }
     }
 } 
